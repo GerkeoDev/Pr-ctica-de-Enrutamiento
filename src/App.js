01981f48fd512ce3a4a7d95002b748e348872de4
GerkeoDev <152994246+GerkeoDev@users.noreply.github.com>
2024-03-20 16:49:01 -0300

    practica-de-enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Link,
+  Routes,
+  Route,
+  useParams
+} from "react-router-dom";
 
+const Home = () => {
+  return <h1>Welcome!</h1>
+}
+const Character = () => {
+  const {character} = useParams()
+  return isNaN(+character) ? <h1>The word is: {character}</h1> : <h1>The number is: {character}!</h1>
+}
+const BlueRed = () => {
+  const {character, color, background} = useParams()
+  return isNaN(+character) 
+    ? <h1 style={{color: color, background: background}}>The word is: {character}</h1> 
+    : <h1 style={{color: color, background: background}}>The number is: {character}</h1>
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/home" element={<Home />}/>
+        <Route path="/:character" element={<Character />}/>
+        <Route path="/:character/:color/:background" element={<BlueRed />}/>
+      </Routes>
     </div>
   );
 }
